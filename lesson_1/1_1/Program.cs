45571ff375adf4174aa57769979f4e6dcc316219
unknown <ul.aeva.vasili.sa@mail.ru>
2024-01-14 16:30:01 +0300

    задача

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Valentina");
+﻿//Console.WriteLine("Valentina");
+
+int[] array = { 2, -4, -7, -3, 9, 3, 5, 0, -7, -66, -55 };
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = array[i] * -1;
+    Console.Write($"{array[i]} ");
+}
